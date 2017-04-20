c32ebe03876cda3571c6d93d83cd00d28bd08066
amninderchahal <amninderchahhal@gmail.com>
2017-04-20 15:52:39 -0400

    Creates TriangleSolver Class

---
+++ Assignment_4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        private static int value;
+        public static void Main(string[] args)
+        {
+            bool chk;
+            string userSelection;
+            do
+            {
+                userSelection = menu();
+                chk = verifyUserSelection(userSelection);
+            } while (!chk);
+
+            if (value == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the three dimensions of triangle\nSide A:");
+                int side_A = checkTriangleSide(Console.ReadLine());
+                Console.WriteLine("Side B:");
+                int side_B = checkTriangleSide(Console.ReadLine());
+                Console.WriteLine("Side C:");
+                int side_C = checkTriangleSide(Console.ReadLine());
+                
+                string output = "\n" + TriangleSolver.analyze(side_A, side_B, side_C);
+                Console.WriteLine(output);
+                step_2:
+                Console.WriteLine("\nEnter 1 to restart or 2 to quit.");
+                int restart = Convert.ToInt32(Console.ReadLine());
+                if (restart == 1)
+                    Main(null);
+                else if (restart == 2)
+                    Environment.Exit(0);
+                else
+                    goto step_2;
+            }
+            else
+                Environment.Exit(0);
+        }
+
+        public static string menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1.Enter triangle dimensions\n2.Exit");
+            return Console.ReadLine();
+        }
+        public static bool verifyUserSelection(string userSelection)
+        {
+            bool check = int.TryParse(userSelection, out value);
+            if (check)
+            {
+                if (value == 1 || value == 2)
+                {
+                    Console.WriteLine(userSelection);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        protected static int checkTriangleSide(String input)
+        {
+            int outVal;
+            String val = input;
+            bool ifInt = int.TryParse(val, out outVal);
+
+            while(!ifInt){
+                Console.WriteLine("Please enter a valid value");
+                val = Console.ReadLine();
+                ifInt = int.TryParse(val, out outVal);
+            }
+            return outVal;
+        }
+    }
+}
---
+++ Assignment_4/TriangleSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    public static class TriangleSolver
+    {
+        public static string analyze(int side_A, int side_B, int side_C)
+        {
+            string result = null;
+
+            if (side_A == 0 || side_B == 0 || side_C == 0)
+                result = "Triangle sides cannot be 0";
+
+            else if (side_A < 0 || side_B < 0 || side_C < 0)
+                result = "Triangle sides cannot negative";
+
+            else if ((side_A + side_B < side_C) || (side_B + side_C < side_A) || (side_A + side_C < side_B))
+                result = "Triangle cannot be formed with the given sides";
+
+            else if (side_A == side_B && side_B == side_C)
+                result = "Triangle is Equilateral";
+                
+            else if (side_A == side_B || side_B == side_C || side_C == side_A)
+                result = "Triangle is Isosceles";
+
+            else if (side_A != side_B && side_B != side_C && side_C != side_A)
+                result = "Triangle is Scalene";
+
+            return result;
+        }
+    }
+}
